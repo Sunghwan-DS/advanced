2d06c01a9c3ca4d65402f8599a68f6f5f2665ea1
Sunghwan-DS <jeonsung02@gmail.com>
2024-02-03 21:50:11 +0900

    Change FieldLogTrace to ThreadLocalLogTrace - Fix Singleton issue

--- src/main/java/jsh/advanced/LogTraceConfig.java
+++ src/main/java/jsh/advanced/LogTraceConfig.java
@@ -1,7 +1,7 @@
 package jsh.advanced;
 
-import jsh.advanced.trace.logtrace.FieldLogTrace;
 import jsh.advanced.trace.logtrace.LogTrace;
+import jsh.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +10,6 @@ public class LogTraceConfig {
 
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 }
