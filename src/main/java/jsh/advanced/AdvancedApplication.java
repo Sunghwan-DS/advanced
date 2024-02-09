b8c144ef6eff826e0ddb72fe95c0d82282ae9f1a
Sunghwan-DS <jeonsung02@gmail.com>
2024-02-09 12:21:43 +0900

    Apply JdkDynamicProxy in Order Process V1 with Filter - LogTraceFilterHandler

--- src/main/java/jsh/advanced/AdvancedApplication.java
+++ src/main/java/jsh/advanced/AdvancedApplication.java
@@ -1,12 +1,12 @@
 package jsh.advanced;
 
 import jsh.proxy.config.AppV2Config;
-import jsh.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
+import jsh.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import({DynamicProxyBasicConfig.class, AppV2Config.class})
+@Import({DynamicProxyFilterConfig.class, AppV2Config.class})
 @SpringBootApplication(scanBasePackages = "jsh.proxy.app")
 public class AdvancedApplication {
 
