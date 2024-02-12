df117840c7827b87464823d688bcbf4947db0d8b
Sunghwan-DS <jeonsung02@gmail.com>
2024-02-12 15:59:28 +0900

    Swapping Objects Using an BeanPostProcessor

--- src/test/java/jsh/proxy/postprocessor/BeanPostProcessorTest.java
+++ src/test/java/jsh/proxy/postprocessor/BeanPostProcessorTest.java
@@ -3,7 +3,9 @@ package jsh.proxy.postprocessor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -13,22 +15,27 @@ public class BeanPostProcessorTest {
 
     @Test
     void basicConfig() {
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(BasicConfig.class);
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(BeanPostProcessorConfig.class);
 
-        //A는 빈으로 등록된다.
-        A a = applicationContext.getBean("beanA", A.class);
-        a.helloA();
+        //beanA 이름으로 B 객체가 빈으로 등록된다.
+        B b = applicationContext.getBean("beanA", B.class);
+        b.helloB();
 
         //B는 빈으로 동록되지 않는다.
-        Assertions.assertThrows(NoSuchBeanDefinitionException.class, () -> applicationContext.getBean("beanB", B.class));
+        Assertions.assertThrows(NoSuchBeanDefinitionException.class, () -> applicationContext.getBean(A.class));
     }
 
     @Slf4j
     @Configuration
-    static class BasicConfig {
+    static class BeanPostProcessorConfig {
         @Bean(name = "beanA")
         public A a() {
             return new A();
+        }
+
+        @Bean
+        public AToBPostProcessor helloPostProcessor() {
+            return new AToBPostProcessor();
         }
     }
 
@@ -43,6 +50,19 @@ public class BeanPostProcessorTest {
     static class B {
         public void helloB() {
             log.info("hello B");
+        }
+    }
+
+    @Slf4j
+    static class AToBPostProcessor implements BeanPostProcessor {
+
+        @Override
+        public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+            log.info("beanName={} bean={}", beanName, bean);
+            if (bean instanceof A) {
+                return new B();
+            }
+            return bean;
         }
     }
 }
