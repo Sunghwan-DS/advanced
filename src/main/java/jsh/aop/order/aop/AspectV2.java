8310406e7a0cfb70e9605e8a2e5f5ff40c1540b6
Sunghwan-DS <jeonsung02@gmail.com>
2024-02-13 08:46:54 +0900

    Separate pointcut in AspectV2 - The pointcut is reusable

--- src/main/java/jsh/aop/order/aop/AspectV2.java
+++ src/main/java/jsh/aop/order/aop/AspectV2.java
@@ -4,14 +4,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Slf4j
 @Aspect
 public class AspectV2 {
 
-    @Around("execution(* jsh.aop.order..*(..))")
+    @Pointcut("execution(* jsh.aop.order..*(..))")
+    private void allOrder(){} //pointcut 시그니처
+
+    @Around("allOrder()")
     public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
-        log.info("[log] {}", joinPoint.getSignature());
+        log.info("[log] {}", joinPoint.getSignature()); //join point 시그니처
         return joinPoint.proceed();
     }
 }
