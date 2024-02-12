2768409ec39455815abfaedb9fe4e110632eb008
Sunghwan-DS <jeonsung02@gmail.com>
2024-02-13 08:36:11 +0900

    Use Aspect in AopTest - aopInfo(): false -> true - Need @Import(AspectV1.class) to register Spring Bean

--- src/test/java/jsh/aop/AopTest.java
+++ src/test/java/jsh/aop/AopTest.java
@@ -3,14 +3,17 @@ package jsh.aop;
 import jsh.advanced.AdvancedApplication;
 import jsh.aop.order.OrderRepository;
 import jsh.aop.order.OrderService;
+import jsh.aop.order.aop.AspectV1;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @Slf4j
+@Import(AspectV1.class)
 @SpringBootTest(classes = AdvancedApplication.class)
 public class AopTest {
 
