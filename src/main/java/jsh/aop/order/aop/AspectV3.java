4e83b3d29262c0aa3f42f6ae9fde5740799d3fba
Sunghwan-DS <jeonsung02@gmail.com>
2024-02-13 13:53:20 +0900

    Copy AspectV2 to create AspectV3

---
+++ src/main/java/jsh/aop/order/aop/AspectV3.java
@@ -0,0 +1,21 @@
+package jsh.aop.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Slf4j
+@Aspect
+public class AspectV3 {
+
+    @Pointcut("execution(* jsh.aop.order..*(..))")
+    private void allOrder(){} //pointcut 시그니처
+
+    @Around("allOrder()")
+    public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("[log] {}", joinPoint.getSignature()); //join point 시그니처
+        return joinPoint.proceed();
+    }
+}
