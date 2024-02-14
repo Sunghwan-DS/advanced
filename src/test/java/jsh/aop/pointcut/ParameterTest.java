42641b0202004bca609e8692284455c932dd31f5
Sunghwan-DS <jeonsung02@gmail.com>
2024-02-14 18:53:38 +0900

    Add args PCD test

--- src/test/java/jsh/aop/pointcut/ParameterTest.java
+++ src/test/java/jsh/aop/pointcut/ParameterTest.java
@@ -2,9 +2,11 @@ package jsh.aop.pointcut;
 
 import jsh.aop.member.MemberService;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,6 +39,17 @@ public class ParameterTest {
             Object arg1 = joinPoint.getArgs()[0];
             log.info("[logArgs1]{}, arg={}", joinPoint.getSignature(), arg1);
             return joinPoint.proceed();
+        }
+
+        @Around("allMember() && args(arg, ..)")
+        public Object logArgs2(ProceedingJoinPoint joinPoint, Object arg) throws Throwable {
+            log.info("[logArgs2]{}, arg={}", joinPoint.getSignature(), arg);
+            return joinPoint.proceed();
+        }
+
+        @Before("allMember() && args(arg, ..)")
+        public void logArgs2(Object arg) {
+            log.info("[logArgs3] arg={}", arg);
         }
     }
 }
