ac2fbd8e928420c67fed54b2bec751233d5fa14a
Sunghwan-DS <jeonsung02@gmail.com>
2024-02-15 18:29:43 +0900

    Add this PDC test in ThisTargetTest

--- src/test/java/jsh/aop/pointcut/ThisTargetTest.java
+++ src/test/java/jsh/aop/pointcut/ThisTargetTest.java
@@ -2,13 +2,22 @@ package jsh.aop.pointcut;
 
 import jsh.aop.member.MemberService;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
+/**
+ * application.properties
+ * spring.aop.proxy-target-class=true   CGLIB
+ * spring.aop.proxy-target-class=false  JDK 동적 프록시
+ */
 @Slf4j
-@SpringBootTest
+@Import(ThisTargetTest.ThisTargetAspect.class)
+@SpringBootTest(properties = "spring.aop.proxy-target-class=false")
 public class ThisTargetTest {
 
     @Autowired
@@ -23,6 +32,20 @@ public class ThisTargetTest {
     @Slf4j
     @Aspect
     static class ThisTargetAspect {
+
+        //부모 타입 허용
+        @Around("this(jsh.aop.member.MemberService)")
+        public Object doThisInterface(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[this-interface] {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+
+        //JDK 기반 프록시의 경우에는 AOP 대상이 아님
+        @Around("this(jsh.aop.member.MemberServiceImpl)")
+        public Object doThis(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[this-impl] {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
 
     }
 }
