f0cbbac5818cb2c73ab5d3cce245b0b8886bcd49
Sunghwan-DS <jeonsung02@gmail.com>
2024-02-15 17:52:08 +0900

    Create ThisTargetTest

---
+++ src/test/java/jsh/aop/pointcut/ThisTargetTest.java
@@ -0,0 +1,28 @@
+package jsh.aop.pointcut;
+
+import jsh.aop.member.MemberService;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+public class ThisTargetTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Test
+    void success() {
+        log.info("memberService Proxy={}", memberService.getClass());
+        memberService.hello("helloA");
+    }
+
+    @Slf4j
+    @Aspect
+    static class ThisTargetAspect {
+
+    }
+}
