64df0969f4cb231ea407108ca22f85189c5d56a0
Sunghwan-DS <jeonsung02@gmail.com>
2024-02-21 09:35:49 +0900

    Injecting itself with a setter method - Impossible from Spring Boot 2.6

--- src/main/java/jsh/aop/internalcall/CallServiceV1.java
+++ src/main/java/jsh/aop/internalcall/CallServiceV1.java
@@ -7,9 +7,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class CallServiceV1 {
 
+
+    // 스프링 부트 2.6 부터는 수정자로 자기 자신 주입이 불가능하다.
+    // spring.main.allow-circular-references=true 설정 필요
+//    private CallServiceV1 callServiceV1;
+//
+//    @Autowired
+//    public void setCallServiceV1(CallServiceV1 callServiceV1) {
+//        this.callServiceV1 = callServiceV1;
+//    }
+
     public void external() {
         log.info("call external");
-        internal(); //내부 메서드 호출(this.internal())
+//        callServiceV1.internal(); //외부 메서드 호출
     }
 
     public void internal() {
